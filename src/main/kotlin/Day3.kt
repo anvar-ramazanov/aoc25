84cb5f2f57daf0ffa9de8d7a8e9d970e60dc4013
Anvar Ramazanov <ramazanov.anvar@gmail.com>
2025-12-03 21:10:58 +0100

    Day3 more kotlin idiomatic

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -1,66 +1,38 @@
 class Day3 : Task {
     override fun solve_part1(): Any {
         return readInput("Day3")
-            .sumOf { getJoltage(it) }
-    }
-
-    private fun getJoltage(line: String): Int {
-        val len = line.length
-
-        var left = line[len - 2]
-        var leftInd = len - 2
-
-        for (i in len -2 downTo 0) {
-            if (line[i] >= left) {
-                left = line[i]
-                leftInd = i
-            }
-        }
-
-        var right = line[leftInd + 1]
-        for (i in leftInd + 1 until len) {
-            if (line[i] >= right) {
-                right = line[i]
-            }
-        }
-
-        return (left + "" + right).toInt()
+            .sumOf { getJoltage(it, 2) }
     }
 
     override fun solve_part2(): Any {
         return readInput("Day3")
-            .sumOf { getJoltage2(it, 12) }
+            .sumOf { getJoltage(it, 12) }
     }
 
-    private fun getJoltage2(line: String, l: Int): Long {
-        val accumulator = mutableListOf<Char>()
-        val len = line.length
+    private fun getJoltage(line: String, maxSlots: Int): Long {
+        val startIndex = line.length - maxSlots
 
+        val leftIndex = (startIndex downTo 0)
+            .reduce { acc, i -> if (line[i] >= line[acc]) i else acc }
 
-        var left = line[len - l]
-        var prevInd = len - l
+        val result = buildList {
+            add(line[leftIndex])
+            var prevIndex = leftIndex
 
-        for (i in len - l downTo 0) {
-            if (line[i] >= left) {
-               left = line[i]
-                prevInd = i
+            repeat(maxSlots - 1) {
+                val remainingSlots = maxSlots - size
+                val searchRange = (prevIndex + 1)..(line.length - remainingSlots)
+
+                val (maxChar, maxIndex) = searchRange
+                    .fold(line[searchRange.first] to searchRange.first) { (maxChar, maxIndex), i ->
+                        if (line[i] > maxChar) line[i] to i else maxChar to maxIndex
+                    }
+
+                add(maxChar)
+                prevIndex = maxIndex
             }
         }
-        accumulator.add(left)
 
-        while (accumulator.size < l) {
-            var cur = line[prevInd + 1]
-            var curInd = prevInd + 1
-            for (i in prevInd + 1 .. len - (l - accumulator.size)) {
-                if (line[i] > cur) {
-                    cur = line[i]
-                    curInd = i
-                }
-            }
-            accumulator.add(cur)
-            prevInd = curInd
-        }
-
-        return accumulator.joinToString("").toLong()
+        return result.joinToString("").toLong()
     }
 }
